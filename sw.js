8f5caa001993d0abfc14590f1e3c22d8030fb759
MfenaKaase <68476762+MfenaKaase@users.noreply.github.com>
2020-09-01 12:19:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const cacheName = 'cache-v1';
+const resourcesToPrecache = [
+    './',
+    './index.html',
+    './style.css',
+    './main.js',
+    './icons/to-do-list.png',
+    './icons/to-do-list1.png',
+    './manifest.webmanifest'
+];
+
+
+self.addEventListener('install', event => {
+    console.log('Install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.addAll(resourcesToPrecache);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Activate event!')
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event!')
+});
